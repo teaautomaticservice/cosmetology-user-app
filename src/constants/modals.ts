9f64676837c02b70dd7ccebc9c6ac9bb882f46e6
DahakaLab <dahakalab@gmail.com>
2025-10-08 15:33:18 +0300

    cashier Fix types for modal

--- src/constants/modals.ts
+++ src/constants/modals.ts
@@ -1,6 +1,5 @@
 import { MessageModal } from '@components/domain/historyMessages/messageModal/MessageModal';
-import { MODALS_TYPE } from '@typings/modals';
 
 export const ModalsMap = {
-  [MODALS_TYPE.HISTORY]: MessageModal,
+  'history': MessageModal,
 };
